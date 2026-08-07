af159d52b46c2102407b5e55028e2cdb28cc8710
Rhythm069 <rhythmacharya19@gmail.com>
2026-08-07 22:15:14 +0545

    Basic_file setup

---
+++ ecommerce/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .import views
+app_name= "ecommerce"
+
+urlpatterns = [
+
+    path('',views.home,name='home'),
+    path('about/',views.about, name ='about'),
+    path('contact/',views.contact, name ='contact'),
+    path('collection/',views.collection, name ='collection')
+
+]
---
+++ ecommerce/views.py
@@ -0,0 +1,21 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    
+    return render(request, "ecommerce/index.html")
+
+def about(request):
+    return render(request,"ecommerce/about.html")
+
+def contact(request):
+   
+    return render(request,"ecommerce/contact.html")
+
+def collection(request):
+   
+    
+    return render(request,"ecommerce/collection.html",)
+
+
