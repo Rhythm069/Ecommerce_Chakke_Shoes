9187ca7bf5e6c7a735adf1323b2edd3cffa8fa04
Rhythm069 <rhythmacharya19@gmail.com>
2026-08-12 22:14:53 +0545

    making models.py

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Product,Customer,Profile,Cart,Order,Hero
+
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Profile)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(Hero)
+
+
+
 # Register your models here.
